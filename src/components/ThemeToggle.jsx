7324d2239def27a6de42c7d8a5cf1cd8c5634006
Joel George <georgejoel050@gmail.com>
2025-05-17 23:22:37 +1000

    Theme toggle working

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,24 +1,40 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
   const [isLightMode, setIsLightMode] = useState(false);
-
+  
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "light") {
+        setIsLightMode(true);
+        document.documentElement.classList.add("light");
+    } else {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.classList.remove("light");    
+        setIsLightMode(false);
+    }
+  }
+  , []);
+    
   const toggleTheme = () => {
     if (isLightMode) {
       document.documentElement.classList.remove("light");
-      localStorage.setItem("theme", "light");
+      localStorage.setItem("theme", "dark");
       setIsLightMode(false);
     } else {
       document.documentElement.classList.add("light");
-      localStorage.setItem("theme", "dark");
+      localStorage.setItem("theme", "light");
       setIsLightMode(true);
     }
   };
 
   return (
     <button
-      onClick={toggleTheme}>
+      onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outlin-hidden")}>
+      
       {isLightMode ? (
         <Moon className="h-6 w-6 text-blue-900" />
       ) : (
